d7205990c7f47f8a62a9d612823f3937d0cd3aeb
Wei-Ning Huang <w@dexon.org>
2019-04-09 22:25:05 +0800

    Setup CI

--- ci/build.sh
+++ ci/build.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+cd $(dirname "$0")/..
+
+if [ -e "ci/deploy_key" ]; then
+  eval $(ssh-agent -s)
+  ssh-add ci/deploy_key
+fi
+
+# Use ssh.
+git remote rm origin
+git remote add origin git@github.com:dexon-foundation/wiki.git
+git fetch origin
+
 git submodule update
 git -C src pull origin master
 git commit -am 'Update src refs'
@@ -13,5 +25,5 @@ git add .
 git commit -am 'Update wiki'
 git push origin gh-pages
 
-git co master
+git checkout master
 git push origin master
